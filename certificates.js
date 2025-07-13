1d1326c37209f77952a2be1e7e2a8c4d2c43756e
prathimay2k <dprathimay2k@gmail.com>
2025-07-13 13:42:56 +0100

    Update certificates.js

--- certificates.js
+++ certificates.js
@@ -11,6 +11,16 @@ const certificates = [
     image:
       "https://drive.google.com/thumbnail?id=1jevBbPsgv4pIFRXE0mSQd-g2obQkBnF-",
     link:
+      "https://forage-uploads-prod.s3.amazonaws.com/completion-certificates/9PBTqmSxAf6zZTseP/io9DzWKe3PTsiS6GG_9PBTqmSxAf6zZTseP_kxkoPGNErASk7mqm3_1751722614066_completion_certificate.pdf"
+  }
+  {
+    id: generateId(),
+    title: "Python Interview Crash Course Certificate",
+    description:
+      "Completed an intensive crash course on Python fundamentals geared toward technical interviews. Covered core concepts such as data types, control flow, functions, OOP, and real-world problem-solving using Python best practices.",
+    image:
+      "https://drive.google.com/thumbnail?id=1jevBbPsgv4pIFRXE0mSQd-g2obQkBnF-",
+    link:
       "https://www.analystbuilder.com/achievements/verify/LIWF1SXY75?r=qzcvJCmTK7hKkFhmW5Ref1OKB"
   }
 ];
